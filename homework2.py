39b2fb7e78318c8098539339aa2083c3446c85bf
SergeiGor <sgorobin@mail.ru>
2024-06-06 12:33:35 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework_count=12
+hour_count=1.5
+course_name="Python"
+hour_per_work=hour_count/homework_count
+print("Курс "+course_name+", всего задач: "+str(homework_count)+", затрачено часов: "+str(hour_count)+", среднее время выполнения:",hour_per_work,"часа.")
---
+++ main.py
@@ -0,0 +1,35 @@
+# Задача «Длина слова»
+
+a="моноколесо"
+print(len(a))
+
+# Задача «Суммы и разности»
+
+first=5
+second=15
+
+summa=first+second
+diff=first-second
+print(summa,diff)
+
+# Задача «Среднее арифметическое»
+
+first=5
+second=15
+third=25
+mean=(first+second+third)/3
+print(mean)
+
+# Задача «Простые строчки»
+
+first_string="Вторник"
+second_string="Понедельник"
+print(second_string+", "+first_string)
+
+# Задача «Сложная формула»
+
+a=2
+b=4
+c=8
+f=(a*b)+(a*c)
+print((f**3)/2)
